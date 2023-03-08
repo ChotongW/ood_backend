27c4f6ed6de873bcf5ab26d320dd4119ae6b4088
Wasin Wasantivong <chotongza@gmail.com>
2023-03-08 22:42:40 +0700

    change database to OOP implement

--- config/db.js
+++ config/db.js
@@ -1,50 +1,54 @@
 const mysql = require("mysql");
 const util = require("util");
 const fs = require("fs");
-const dotenv = require("dotenv");
-dotenv.config();
+const config = require("./env.js");
+// const path = require("path");
+// const dotenv = require("dotenv");
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.database,
-  port: 3306,
-  ssl: { ca: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") },
-});
+// dotenv.config({
+//   path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
+// });
 
-db.getConnection((err, connection) => {
-  if (err) {
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      console.error("Database connection was closed.");
-    }
-    if (err.code === "ER_CON_COUNT_ERROR") {
-      console.error("Database has too many connections.");
-    }
-    if (err.code === "ECONNREFUSED") {
-      console.error("Database connection was refused.");
-    }
+class Database {
+  constructor() {
+    this.pool = mysql.createPool({
+      host: config.DB_HOST,
+      user: config.DB_USER,
+      password: config.DB_PASS,
+      database: config.database,
+      port: 3306,
+      // ssl: { ca: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") },
+    });
+    this.pool.getConnection((err, connection) => {
+      if (err) {
+        if (err.code === "PROTOCOL_CONNECTION_LOST") {
+          console.error("Database connection was closed.");
+        }
+        if (err.code === "ER_CON_COUNT_ERROR") {
+          console.error("Database has too many connections.");
+        }
+        if (err.code === "ECONNREFUSED") {
+          console.error("Database connection was refused.");
+        }
+      }
+      if (connection) connection.release();
+    });
+    this.pool.query = util.promisify(this.pool.query);
   }
 
-  if (connection) connection.release();
+  async query(sql, params) {
+    return await this.pool.query(sql, params);
+  }
 
-  return;
-});
+  // async query(sql, params, doErr, doSucc) {
+  //   this.pool.query(sql, params, (err, result) => {
+  //     if (!err) {
+  //       doSucc(result);
+  //     } else {
+  //       doErr(err);
+  //     }
+  //   });
+  // }
+}
 
-db.query = util.promisify(db.query);
-
-const queryDB = async (sql, params) => {
-  return await db.query(sql, params);
-};
-
-// const queryDB = async (sql, params, doErr, doSucc) => {
-//   db.query(sql, params, (err, result) => {
-//     if (!err) {
-//       doSucc(result);
-//     } else {
-//       doErr(err);
-//     }
-//   });
-// };
-
-module.exports = queryDB;
+module.exports = new Database();
