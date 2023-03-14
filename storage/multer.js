e2cf51144fc20dec87762e9184a6a50c04dd97a6
Wasin Wasantivong <chotongza@gmail.com>
2023-03-15 02:09:22 +0700

    roll back old structure

---
+++ storage/multer.js
@@ -0,0 +1,20 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "./upload");
+  },
+  filename: (req, file, callback) => {
+    callback(
+      null,
+      "file-" +
+        Date.now() +
+        "." +
+        file.originalname.split(".")[file.originalname.split(".").length - 1]
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
